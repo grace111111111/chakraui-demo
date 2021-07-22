ef6e3e801ba01b87f684d0f19789cc4595499d57
GraceTTT <1157166306@qq.com>
2021-07-22 17:55:41 +0800

    更新

--- src/components/Form.js
+++ src/components/Form.js
@@ -6,24 +6,32 @@ import {
   TabPanel,
   TabPanels,
   Box,
+  Image,
+  useColorModeValue
 } from "@chakra-ui/core";
 import SignUp from "./SignUp";
 import SignIn from "./SignIn";
 
-export default function Form({props}) {
+import chakraUILight from "../assets/images/chakra-ui-light.png";
+import chakraUIDark from '../assets/images/chakra-ui-dark.png';
+
+export default function Form() {
+  const bgColor = useColorModeValue('gray.200', 'gray.700');
+  const chakraUI = useColorModeValue(chakraUILight, chakraUIDark)
   return (
-    <Box backgroundColor="gray.200" p={3} w="100%" boxShadow="lg" borderRadius="lg" p="80px 30px">
+    <Box bgColor={bgColor} p={3} w="100%" boxShadow="lg" borderRadius="lg">
+      <Image w="250px" mx="auto" mt="2" mb="6" src={chakraUI} />
       <Tabs isFitted>
         <TabList>
-          <Tab _focus={{ boxShadow: "none" }}>登录</Tab>
           <Tab _focus={{ boxShadow: "none" }}>注册</Tab>
+          <Tab _focus={{ boxShadow: "none" }}>登录</Tab>
         </TabList>
         <TabPanels>
           <TabPanel>
-            <SignIn />
+            <SignUp />
           </TabPanel>
           <TabPanel>
-            <SignUp />
+            <SignIn />
           </TabPanel>
         </TabPanels>
       </Tabs>
